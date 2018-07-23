6a5aef1106c67e1be11c96df16a3c232c060a902
Jakob <jakob.sjudin@gmail.com>
2018-07-23 19:23:21 +0200

    Added error handling

---
+++ sgs_reminder_new.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import pyautogui
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.sgsstudentbostader.se/')
+driver.find_element_by_class_name('mina-sidor-button').click()
+driver.find_element_by_class_name('mypages-image').click()
