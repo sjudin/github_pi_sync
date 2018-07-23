f7486d313c014faa7f30f6f35564799780a34dc0
Jakob <jakob.sjudin@gmail.com>
2018-07-23 19:49:59 +0200

    Added error handling

--- sgs_reminder_new.py
+++ sgs_reminder_new.py
@@ -3,7 +3,7 @@ import pyautogui
 
 
 
-driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+driver = webdriver.Chrome()
 
 driver.get('https://www.sgsstudentbostader.se/')
 driver.find_element_by_class_name('mina-sidor-button').click()
