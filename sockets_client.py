574030a6e0d5439c10eeb2ad8b39b0442d985292
Jakob <jakob.sjudin@gmail.com>
2018-08-06 20:58:36 +0200

    Added error handling

--- sockets_client.py
+++ sockets_client.py
@@ -1,9 +1,13 @@
 import socket
 
+SERVER_IP = '192.168.0.102'
+
 s = socket.socket()
 host = socket.gethostname()
-port = 1500
+PORT_NUMBER = 1500
 
-s.connect((host, port))
-print(s.recv(1024))
-s.close()
+s.connect((host, PORT_NUMBER))
+
+print("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
+
+s.send('This is a message')
