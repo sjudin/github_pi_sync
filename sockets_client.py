f38566068f5dd6538c1829a92e00fe79853e62c1
Jakob <jakob.sjudin@gmail.com>
2018-08-06 21:12:11 +0200

    Added error handling

--- sockets_client.py
+++ sockets_client.py
@@ -12,5 +12,6 @@ s.connect((SERVER_IP, PORT_NUMBER))
 print("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
 
 print(s.recv(1024))
-s.send(str(datetime.datetime.now()).encode())
+
+s.send(str(datetime.datetime.now()).encode('UTF-8'))
 s.close()
