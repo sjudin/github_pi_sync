6730a2286bc0a1c7fc4e1e14a74b4cfc1e15d607
Jakob <jakob.sjudin@gmail.com>
2018-12-28 18:44:39 +0100

    Now works with new website

--- sgs_reminder_new.py
+++ sgs_reminder_new.py
@@ -5,24 +5,29 @@ import time
 import datetime
 
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-
+#driver = webdriver.Chrome()
+driver.fullscreen_window()
 
 driver.get('https://www.sgsstudentbostader.se/')
-driver.find_element_by_class_name('mina-sidor-button').click()
-driver.find_element_by_class_name('mypages-image').click()
+driver.find_element_by_class_name('nav-link').click()
+driver.find_element_by_name('User').send_keys(sys.argv[1])
+driver.find_element_by_name('Password').send_keys(sys.argv[2])
+
+driver.find_element_by_class_name('btn-primary').click()
 
 time.sleep(10)
-pyautogui.click(x=137, y=487)
-pyautogui.typewrite(sys.argv[1])
-pyautogui.click(x=137, y=553)
-pyautogui.typewrite(sys.argv[2])
-time.sleep(1)
 
-driver.find_element_by_class_name('sgs-link-to').click()
-
-time.sleep(10)
+# pyautogui.click(x=137, y=487)
+# pyautogui.typewrite(sys.argv[1])
+# pyautogui.click(x=137, y=553)
+# pyautogui.typewrite(sys.argv[2])
+# time.sleep(1)
+#
+# driver.find_element_by_class_name('sgs-link-to').click()
+#
+# time.sleep(10)
 driver.save_screenshot('updated_latest.png')
-
+#
 print(str(sys.argv[0]) + 'finished at ' + str(datetime.datetime.now()))
 
 driver.close()
